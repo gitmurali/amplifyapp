c5f503b7d779313f18dc3ae7924d91ba7910455c
gitmurali <murali.nutalapatii@gmail.com>
2023-06-07 23:21:16 +0100

    update ui components

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyapp6a711a9e": {
       "AppClientID": "string",
